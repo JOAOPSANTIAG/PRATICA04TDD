6a1f21edb6b89e14b3d1a006457b0ee79889ccf5
JOAO PEDRO FERREIRA SANTIAGO <joaosantiago.jp@gmail.com>
2025-05-15 22:45:49 -0300

    Teste inicial TDD

---
+++ pratica04/src/pratica04/testefatorial.java
@@ -0,0 +1,14 @@
+package pratica04;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testefatorial {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
